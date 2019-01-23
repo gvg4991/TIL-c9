8ab3cf263076976c10e3a2655682bee96f54aafc
minho seo <14.73oo6o19@gmail.com>
2019-01-23 08:47:11 +0000

    D16 | 190123 | CRUD & ORM

--- flask/orm/app.py
+++ flask/orm/app.py
@@ -25,3 +25,48 @@ class User(db.Model):
     
     def __repr__(self):
         return f'<User {self.id}: {self.username}, {self.email}>'
+        
+        
+# [Create]
+# INSERT INTO users (username, email) VALUES ('gvg4991','14.73oo6o19@gmail.com')
+# user=User(username='gvg4991',email='14.73oo6o19@gmail.com')
+# db.session.add(user)
+# db.session.commit()
+# User.query.all()  (user의 id값을 가지고옴)
+
+# [Read]
+# SELECT * FROM users;
+# users = User.query.all() #복수
+
+# SELECT * FROM users WHERE username='gvg4991';
+# users = User.query.filter_by(username='gvg4991').all()
+
+# SELECT * FROM users WHERE username='gvg4991' LIMIT 1;
+# user = User.query.filter_by(username='gvg4991').first()
+
+# SELECT * FROM users WHERE id=2 LIMIT 1;
+# user = User.query.get(2)
+# primary key만 get으로 가져올 수 있음.
+
+# SELECT * FROM users WHERE email LIKE '%water%';
+# users = User.query.filter(User.email.like("%water%")).all()
+
+# ORDER
+# users = User.query.limit(1).all()
+
+# OFFSET
+# users = User.query.offset(2).all()
+
+# ORDER + LIMIT + OFFSET
+# users = User.query.order_by(User.username).limit(1).offset(2).all()
+
+# [DELETE]
+# DELETE FROM users WHERE id=1;
+# user = User.query.get(1)  (제거시킬 데이터 번호를 입력: 1)
+# db.session.delete(user)
+# db.session.commit()
+
+# [UPDATE]
+# UPDATE users SET username='yo' WHERE id=2;
+# user = username = 'yo'
+# db.session.commit()
