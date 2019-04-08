6694f7791171c63e2f3774348141ea71b204f3a1
minho seo <14.73oo6o19@gmail.com>
2019-04-08 08:49:29 +0000

    190408 | Django insta

---
+++ django/insta/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('',views.list, name='list'),
+    path('create/', views.create, name='create'), #주소에 대한이름을 name으로 지정
+    path('<int:post_id>/delete/', views.delete, name='delete'), #앞쪽에는 변수에 들어오는 인풋 <타입:변수명>
+    ]
---
+++ django/insta/posts/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
+from .models import Post
+
+
+# Create your views here.
+
+
+def list(request):
+    posts = Post.objects.all()
+    return render(request,'posts/list.html', {'posts':posts})
+
+
+
+def create(request): #첫번째는 request를 인자로 받음
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save() #forms에서 설정해 줘서 바로 post에 저장할 수 있음
+            return redirect('posts:list')
+    else:
+        post_form = PostForm()
+    return render(request, 'posts/create.html', {'post_form':post_form}) #여러개 app을 만들 경우, posts라는 앱이름으로 폴더명 경로를 넣어줌
+    
+    
+
+def delete(request, post_id): #어떤 게시물을 삭제할지 받아와야함(주소를통해 받아올 포스트의 아이디)
+    # post = Post.objects.get(id=post_id) #id값이 포스트 카드인 데이터를 불러옴 (pk프라이머리키보다 id가 많이쓰임)
+    post = get_object_or_404(Post, id=post_id) #메소드의 반환값이 포스트가 됨(게시물이 없으면 404에러 발생)
+    post.delete()
+    return redirect('posts:list')
